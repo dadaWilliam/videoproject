2bebf3e58cc178120b867bfb605aae851fbe74e6
dadaWilliam <dadaWilliam811@icloud.com>
2022-05-17 17:12:15 +0800

    version 2.0

---
+++ videoproject/urls.py
@@ -0,0 +1,34 @@
+"""  URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import re_path
+from video import views
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/',include('users.urls')),
+    path('myadmin/', include('myadmin.urls')),
+    path('video/',include('video.urls')),
+    path('comment/',include('comment.urls')),
+    path('', views.IndexView.as_view(), name='home'), # 默认首页
+    re_path('^static/(?P<path>.*)$', serve, {"document_root": settings.STATIC_ROOT}),
+    re_path('^upload/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
