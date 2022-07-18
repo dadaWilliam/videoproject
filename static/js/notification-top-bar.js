adcdb7a60cb8763ba413e81598b94744c9b0a954
Match <thebellm@outlook.com>
2022-07-18 14:00:08 +0800

    修改公告栏图标

--- static/js/notification-top-bar.js
+++ static/js/notification-top-bar.js
@@ -6,15 +6,16 @@ function get_latest_system_notification(url) {
             if (!data.content) {
                 return;
             }
-            $(".notification-top-bar-container").css("height", "40px");
-            $(".notification-top-bar").css("z-index", "9999");
+            // $(".notification-top-bar-container").css("height", "40px");
+            
             $(".notification-top-bar p").text(data.content);
             const background_color = data.category === "hint" ? "#1ABC9C" : "#FF6347";
             $(".notification-top-bar").css("background", background_color);
-            $('.ui.menu').fadeIn(500);
+            $(".notification-top-bar").css("z-index", "9999");
+            $(".notification-top-bar").fadeIn(500);
+            // $('.ui.menu').fadeIn(0);
             $('.ui.container').fadeIn(500);
-            
-            
+            $('.ui.footer').fadeIn(1000);
         }
     });
 }
